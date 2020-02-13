7221401623c2a911c5d7a066af019160304a9fef
Ivan Stefanyshyn <ivanstefanushun@gmail.com>
2020-02-13 11:01:16 +0200

    feat: added page for create resume

---
+++ src/pages/CreateResumePage/CreateResumePage.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import style from './CreateResumePage.module.css';
+
+class CreateResumePage extends React.Component{ 
+    
+    constructor(props){
+        super(props);
+        this.state = {
+        }
+    }
+
+
+    render = ()=> {
+        return (
+            <div>          
+            </div>
+        );
+    }
+}
+export default CreateResumePage;
